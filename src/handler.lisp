a2e2e84db45957ab7ca615c0f4b9a2f07e2e3fbd
Evan MacTaggart <evan@localhost.localdomain>
2019-09-19 13:27:06 -0600

    disaster!

--- src/handler.lisp
+++ src/handler.lisp
@@ -120,7 +120,7 @@
        (:meta :charset "utf-8")
        (:meta :name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no")
        (:link :rel "stylesheet"
-              :href ht"tps://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+              :href "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
               :integrity "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
               :crossorigin "anonymous")
 
@@ -173,6 +173,7 @@
 
              (:script :src "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" :integrity "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" :crossorigin "anonymous")
              (:script :src "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" :integrity "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" :crossorigin "anonymous")))))
+
 
 (defun profile-handler ()
   (page-template (:title "Welcom to the Lounge")
