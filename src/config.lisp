67934337b3127fdc86043e015bd894d7db80c109
Evan <evan.mactaggart@gmail.com>
2022-01-24 19:00:05 -0600

    [config] - avility to comment out configurations; naming convention cleanup

--- src/config.lisp
+++ src/config.lisp
@@ -1,12 +1,14 @@
 (defpackage :config
   (:use :cl)
-  (:export :get-config :get-config-or-error :load-configs))
+  (:export :get-config :get-config-or-error :load-configs)
+  (:import-from :str :starts-with?))
 
 (in-package :config)
 
-(defvar configs (make-hash-table :test 'equal))
+(defvar *configs* nil)
 
 (defun load-configs (config-file)
+  (setf *configs* (make-hash-table :test 'equal))
   (when (not (uiop:file-exists-p config-file))
     (error "~a" (concatenate 'string "Cannot find configuration file: " config-file)))
   (with-open-file (stream config-file)
@@ -14,10 +16,11 @@
           while line
           do
              (destructuring-bind (key value) (split-sequence:split-sequence #\= line)
-               (setf (gethash key configs) (string-trim " " value))))))
+               (unless (starts-with? "#" (str:trim-left key))
+                 (setf (gethash key *configs*) (string-trim " " value)))))))
 
 (defun get-config (config-name)
-  (gethash config-name configs))
+  (gethash config-name *configs*))
 
 (defun get-config-or-error (config-name)
   (let ((config (get-config config-name)))
