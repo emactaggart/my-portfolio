bebdbea676b5d5d73f2e3c689ab5085d7de9ac91
Evan <evan.mactaggart@gmail.com>
2022-01-28 11:01:20 -0600

    [build] - running prod locally on a non-privilaged port

--- build/4.run-prod-local.sh
+++ build/4.run-prod-local.sh
@@ -9,6 +9,6 @@ fi
 # To run locally
 # docker run -p 8080:80 --entrypoint sh --interactive --tty my-portfolio:alpine
 
-docker run -p 80:80 --entrypoint sh --interactive --tty my-portfolio:alpine -c /root/my-portfolio.exe
+docker run -p 8080:80 --entrypoint sh --interactive --tty my-portfolio:alpine -c /root/my-portfolio.exe
 
 popd
